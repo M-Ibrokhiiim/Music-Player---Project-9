051296fadb53251841fe2e310e9c2b6392c8fcb6
M_Ibrokhim <ibrokhimmuysinov@gmail.com>
2025-03-26 12:16:06 +0300

    70% done!

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const music=document.getElementById('music')
 const music_range=document.getElementById('music-range-input')
 const ctrl_icons=document.getElementById('control-icons')
+const pause_play=document.getElementById('playAndPause')
 
 
 music.onloadedmetadata=function(){
@@ -8,20 +9,29 @@ music.onloadedmetadata=function(){
     music_range.value=music.currentTime
 }
 
-ctrl_icons.addEventListener('click',function(e){
-    if(e.target.classList.contains('fa-play')){
+ 
+function playMusic(){
+    if(pause_play.classList.contains('fa-play')){
         music.play()
-        e.target.classList.add('fa-pause')
-        e.target.classList.remove('fa-play')
+        pause_play.classList.add('fa-pause')
+        pause_play.classList.remove('fa-play')
     }else{
         music.pause()
-        e.target.classList.remove('fa-pause')
-        e.target.classList.add('fa-play')
+        pause_play.classList.remove('fa-pause')
+        pause_play.classList.add('fa-play')
     }
-})
+}
 
 if(music.play()){
-    setInterval(()=>{
-        music_range.value=music.currentTime
-    },500)
+setInterval(() => {
+    music_range.value=music.currentTime
+},600);
+     
+}
+music_range.onchange=function(){
+    music.play()
+    music.currentTime=music_range.value
+    pause_play.classList.remove('fa-play')
+    pause_play.classList.add('fa-pause')   
+
 }
